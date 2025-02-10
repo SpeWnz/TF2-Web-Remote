8be52f5f55abd2b7f8221de602154db980f756a7
Stormix-dev <gianmarco.simeoni.lsa@gmail.com>
2025-02-10 12:36:34 +0100

    Modifiche al file main.rs e Cargo.toml per allineamento

---
+++ rust-map-downloader/target/debug/deps/rust_map_downloader-306d5837328e9161.d
@@ -0,0 +1,5 @@
+C:\Users\gianm\Desktop\Progetti NotVoid\TF2-Web-Remote\rust-map-downloader\target\debug\deps\librust_map_downloader-306d5837328e9161.rmeta: src/main.rs
+
+C:\Users\gianm\Desktop\Progetti NotVoid\TF2-Web-Remote\rust-map-downloader\target\debug\deps\rust_map_downloader-306d5837328e9161.d: src/main.rs
+
+src/main.rs:
---
+++ rust-map-downloader/target/debug/deps/rust_map_downloader-541a4d0a3ba3c8e4.d
@@ -0,0 +1,5 @@
+C:\Users\gianm\Desktop\Progetti NotVoid\TF2-Web-Remote\rust-map-downloader\target\debug\deps\librust_map_downloader-541a4d0a3ba3c8e4.rmeta: src/main.rs
+
+C:\Users\gianm\Desktop\Progetti NotVoid\TF2-Web-Remote\rust-map-downloader\target\debug\deps\rust_map_downloader-541a4d0a3ba3c8e4.d: src/main.rs
+
+src/main.rs:
