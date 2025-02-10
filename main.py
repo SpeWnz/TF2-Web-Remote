18f9e3934f84aa891fe19e865a7747ffd2b2e12f
galvanizedheart <andreadistefano98@gmail.com>
2025-02-10 10:43:03 +0100

    added support for yaml configuration file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, render_template
 import valve.rcon
+import yaml
+
+with open('config.yml', 'r') as file:
+    server_conf = yaml.safe_load(file)
 
 app = Flask(__name__)
-RCON_PASSWORD = "GROSISIMI PENNONI"
-RCON_ADDRESS = ("192.168.1.241",27015)
+RCON_PASSWORD = server_conf['valve']['password']
+RCON_ADDRESS = (server_conf['valve']['address'],server_conf['valve']['port'])
 _RCON = valve.rcon.RCON(RCON_ADDRESS,RCON_PASSWORD)
 
 @app.route('/')
@@ -26,4 +30,4 @@ if __name__ == '__main__':
     
     _RCON.connect()
     _RCON.authenticate()
-    app.run(debug=True,host="0.0.0.0",port="8910")
+    app.run(debug=True,host=server_conf['web']['address'],port=server_conf['web']['port'])
