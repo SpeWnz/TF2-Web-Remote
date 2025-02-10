71c0442944ffec9f7649a152b464a523b76e59c3
galvanizedheart <andreadistefano98@gmail.com>
2025-02-10 02:07:11 +0100

    title extravagance

--- static/script.js
+++ static/script.js
@@ -27,3 +27,26 @@ async function rconExec(button) {
         });
 }
 
+addEventListener("load", (event) => {
+    var titles=[
+      "Welcome!", 
+      "Last one alive, lock the door!", 
+      "STOOOOP PLEASE", 
+      "MEDIC!", 
+      "Overtime!", 
+      "HUDDAH HUDDAH HUH", 
+      "Beep boop you are a maggot",
+      "Ohhhh, I'm going to liquify ya",
+      "Ohhhhhhhhhhhhhhhhh nooooooooooooooooooooooooooo, little cart is moving!",
+      "WHY IS THE CART NOT MOVING?!?!?!?!",
+      "I got a bucket of chicken",
+      "Damnit Damnit Damnit Damnit!",
+      "Nice job pardner",
+      "Pornography"
+    ];
+
+    document.getElementById("title").innerHTML = titles[Math.floor(Math.random()*titles.length)];
+
+  }
+);
+
